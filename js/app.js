62de970db1da1a92c0951694d076b294d52c0635
faizan517 <faizanpervaiz517@gmail.com>
2023-03-02 19:31:32 +0500

    movie-search

---
+++ js/app.js
@@ -0,0 +1,63 @@
+const baseUrl ='https://api.themoviedb.org/3'
+const key = "api_key=384ad7ece83317e3356e866845658ac0"
+const api =
+  baseUrl + "/trending/all/day?" +key ;
+const img_url = "https://image.tmdb.org/t/p/w500";
+
+const searchUrl = baseUrl +"/search/movie?"+ key
+
+function movieApi(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showMoives(data.results));
+}
+movieApi(api);
+
+const main = document.getElementById("main");
+
+function showMoives(data) {
+  main.innerHTML=''
+  data.forEach((movies) => {
+    const { name, title, poster_path, vote_average } = movies;
+    //movieContainer
+    const movieConatiner = document.createElement("div");
+    movieConatiner.setAttribute("class", "movieContainer ");
+
+    //moviemoviePoster
+    const moviePoster = document.createElement("div");
+    moviePoster.setAttribute("class", "movie");
+    var poster = document.createElement("div");
+    poster.innerHTML = `<img src="${
+      img_url + poster_path
+    }" class="moviePoster">`;
+    moviePoster.appendChild(poster);
+    movieConatiner.appendChild(moviePoster);
+
+    //moviemovieName
+    const movieName = document.createElement("div");
+    movieName.setAttribute("class", "movieName");
+    movieName.innerText = `${title || name}`;
+    movieConatiner.appendChild(movieName);
+
+    const rating = document.createElement("div");
+    rating.setAttribute("class", "rating");
+    rating.innerHTML = `<p>${parseFloat(vote_average).toFixed(1)}</p>`;
+    movieConatiner.appendChild(rating);
+
+    main.append(movieConatiner);
+  });
+}
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  const searchValue = search.value
+  // console.log("🚀 ~ file: app.js:57 ~ form.addEventListener ~ searchValue:", searchValue)
+  if (searchValue) {
+    movieApi(searchUrl+'&query='+searchValue)
+  }else{
+    movieApi(api)
+  }
+})
